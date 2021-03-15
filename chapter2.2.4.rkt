66e7883d3dc97e35f5ecfb4e03bc4c1855f2936c
Mark Shevchenko <markshevchenko@gmail.com>
2021-03-15 23:01:34 +0300

    Make exercises til 2.46

--- chapter2.2.4.rkt
+++ chapter2.2.4.rkt
@@ -25,3 +25,89 @@
     (make-segment (make-vect 0.40 0.00) (make-vect 0.50 0.30))
     (make-segment (make-vect 0.50 0.30) (make-vect 0.60 0.00)))))
 
+; Exercise 2.44
+
+(define (flipped-pairs painter)
+  (let ((painter2 (beside painter (flip-vert painter))))
+    (below painter2 painter2)))
+
+(define (right-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (right-split painter (- n 1))))
+        (beside painter (below smaller smaller)))))
+
+(define (up-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (right-split painter (- n 1))))
+        (below painter (beside smaller smaller)))))
+
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left (beside up up))
+              (bottom-right (below right right))
+              (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+
+; (paint (corner-split einstein 4))
+
+(define (square-limit painter n)
+  (let ((quarter (corner-split painter n)))
+    (let ((half (beside (flip-horiz quarter) quarter)))
+      (below (flip-vert half) half))))
+
+(define (square-of-four tl tr bl br)
+  (lambda (painter)
+    (let ((top (beside (tl painter) (tr painter)))
+          (bottom (beside (bl painter) (br painter))))
+      (below bottom top))))
+
+(define (flipped-pairs2 painter)
+  (let ((combine4 (square-of-four identity flip-vert
+                                  identity flip-vert)))
+    (combine4 painter)))
+
+(define (square-limit2 painter n)
+  (let ((combine4 (square-of-four flip-horiz identity
+                                  rotate180 flip-vert)))
+   (combine4 (corner-split painter n))))
+
+; Exercise 2.45
+
+(define (split first second)
+  (define (rec-split painter n)
+    (if (= n 0)
+        painter
+        (let ((smaller (rec-split painter (- n 1))))
+          (first painter (second smaller smaller)))))
+  
+  rec-split)
+
+(define right-split2 (split beside below))
+(define up-split2 (split below beside))
+
+; Exercise 2.46
+
+(define xcor-vect car)
+(define ycor-vect cdr)
+
+(define (add-vect v1 v2)
+  (make-vect (+ (xcor-vect v1) (xcor-vect v2)) (+ (ycor-vect v1) (ycor-vect v2))))
+; > (add-vect (make-vect 1 2) (make-vect 3 4))
+; '(4 . 6)
+
+(define (sub-vect v1 v2)
+  (make-vect (- (xcor-vect v1) (xcor-vect v2)) (- (ycor-vect v1) (ycor-vect v2))))
+; > (sub-vect (make-vect 3 4) (make-vect 1 2))
+; '(2 . 2)
+
+(define (scale-vect s v)
+  (make-vect (* s (xcor-vect v)) (* s (ycor-vect v))))
+; > (scale-vect 10 (make-vect 1 2))
+; '(10 . 20)
+
