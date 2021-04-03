a31c31dbfb62b3f777da013dc1db64e3c3798467
Mark Shevchenko <markshevchenko@gmail.com>
2021-04-03 05:19:12 +0300

    Make exercises til 2.73

--- chapter2.4.rkt
+++ chapter2.4.rkt
@@ -1,5 +1,41 @@
 #lang sicp
 
+; https://stackoverflow.com/a/40283917/1051621
+
+(define (make-table)
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (cdr record)
+                  false))
+            false)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1
+                                  (cons key-2 value))
+                            (cdr local-table)))))
+      'ok)    
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation - TABLE" m))))
+    dispatch))
+
+(define operation-table (make-table))
+(define get (operation-table 'lookup-proc))
+(define put (operation-table 'insert-proc!))
+
 ; Chapter 2.4.1
 
 (define (add-complex z1 z2)
@@ -59,37 +95,183 @@
 (define (make-from-mag-ang-polar r a)
   (attach-tag 'polar (cons r a)))
 
-(define (real-part z)
+(define (real-part.1 z)
   (cond ((rectangular? z)
          (real-part-rectangular (contents z)))
         ((polar? z)
          (real-part-polar (contents z)))
         (else (error "Unknown type -- REAL-PART " z))))
 
-(define (imag-part z)
+(define (imag-part.1 z)
   (cond ((rectangular? z)
          (imag-part-rectangular (contents z)))
         ((polar? z)
          (imag-part-polar (contents z)))
         (else (error "Unknown type -- IMAG-PART " z))))
 
-(define (magnitude z)
+(define (magnitude.1 z)
   (cond ((rectangular? z)
          (magnitude-rectangular (contents z)))
         ((polar? z)
          (magnitude-polar (contents z)))
         (else (error "Unknown type -- MAGNITUDE " z))))
 
-(define (angle z)
+(define (angle.1 z)
   (cond ((rectangular? z)
          (angle-rectangular (contents z)))
         ((polar? z)
          (angle-polar (contents z)))
         (else (error "Unknown type -- ANGLE " z))))
 
-(define (make-from-real-imag x y)
+(define (make-from-real-imag.1 x y)
   (make-from-real-imag-rectangular x y))
-(define (make-from-mag-ang r a)
+(define (make-from-mag-ang.1 r a)
   (make-from-mag-ang-polar r a))
 
 ; Chapter 2.4.3
+
+(define (install-rectangular-package)
+  (define (real-part z) (car z))
+  (define (imag-part z) (cdr z))
+  (define (make-from-real-imag x y) (cons x y))
+  (define (magnitude z)
+    (sqrt (+ (square (real-part z))
+             (square (imag-part z)))))
+  (define (angle z)
+    (atan (imag-part z) (real-part z)))
+  (define (make-from-mag-ang r a)
+    (cons (* r (cos a)) (* r (sin a))))
+
+  (define (tag x) (attach-tag 'rectangular x))
+  (put 'real-part '(rectangular) real-part)
+  (put 'imag-part '(rectangular) imag-part)
+  (put 'magnitude '(rectangular) magnitude)
+  (put 'angle '(rectangular) angle)
+  (put 'make-from-real-imag 'rectangular
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'rectangular
+       (lambda (x y) (tag (make-from-mag-ang x y))))
+
+  'done)
+
+(define (install-polar-package)
+  (define (magnitude z) (car z))
+  (define (angle z) (cdr z))
+  (define (make-from-mag-ang r a) (cons r a))
+  (define (real-part z)
+    (* (magnitude z) (cos (angle z))))
+  (define (imag-part z)
+    (* (magnitude z) (sin (angle z))))
+  (define (make-from-real-imag x y)
+    (cons (sqrt (+ (square x) (square y)))
+          (atan y x)))
+
+  
+  (define (tag x) (attach-tag 'polar x))
+  (put 'real-part '(polar) real-part)
+  (put 'imag-part '(polar) imag-part)
+  (put 'magnitude '(polar) magnitude)
+  (put 'angle '(polar) angle)
+  (put 'make-from-real-imag 'polar
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'polar
+       (lambda (x y) (tag (make-from-mag-ang x y))))
+  'done)
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (error "No methods for types -- APPLY-GENERIC " (list op type-tags))))))
+
+(define (real-part z) (apply-generic 'real-part z))
+(define (imag-part z) (apply-generic 'imag-part z))
+(define (magnitude z) (apply-generic 'magnitude z))
+(define (angle z) (apply-generic 'angle z))
+
+(define (make-from-real-imag x y)
+  ((get 'make-from-real-imag 'rectangular) x y))
+
+(define (make-from-mag-ang r a)
+  ((get 'make-from-mag-ang 'polar) r a))
+
+; > (install-rectangular-package)
+; done
+; > (install-polar-package)
+; done
+; > (make-from-real-imag 3 2)
+; (rectangular 3 . 2)
+; > (make-from-mag-ang 2 3)
+; (polar 2 . 3)
+
+; Exercise 2.73
+
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        (else ((get 'deriv (operator exp)) (operands exp)
+                                           var))))
+
+(define (install-deriv-sum)
+  (define (=number? exp num)
+    (and (number? exp) (= exp num)))
+  (define (make-sum a1 a2)
+    (cond ((=number? a1 0) a2)
+          ((=number? a2 0) a1)
+          ((and (number? a1) (number? a2)) (+ a1 a2))
+          (else (list '+ a1 a2))))
+  (define (addend s) (car s))
+  (define (augend s) (cadr s))
+  (define (deriv-sum operands var)
+    (make-sum (deriv (addend operands) var)
+              (deriv (augend operands) var)))
+
+  (put 'deriv '+ deriv-sum)
+
+  'done)
+; > (install-deriv-sum)
+; done
+; > (deriv '(+ x y) 'x)
+; 1
+; > (deriv '(+ x x) 'x)
+; 2
+
+(define (install-deriv-product)
+  (define (=number? exp num)
+    (and (number? exp) (= exp num)))
+  (define (make-sum a1 a2)
+    (cond ((=number? a1 0) a2)
+          ((=number? a2 0) a1)
+          ((and (number? a1) (number? a2)) (+ a1 a2))
+          (else (list '+ a1 a2))))
+  (define (make-product m1 m2)
+    (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+          ((=number? m1 1) m2)
+          ((=number? m2 1) m1)
+          ((and (number? m1) (number? m2)) (* m1 m2))
+          (else (list '* m1 m2))))
+  (define (multiplier s) (car s))
+  (define (multiplicand s) (cadr s))
+  (define (deriv-product operands var)
+    (make-sum
+     (make-product (multiplier operands)
+                   (deriv (multiplicand operands) var))
+     (make-product (deriv (multiplier operands) var)
+                          (multiplicand operands))))
+
+  (put 'deriv '* deriv-product)
+
+  'done)
+; > (install-deriv-product)
+; done
+; > (install-deriv-sum)
+; done
+; > (deriv '(* 2 (+ x 3)) 'x)
+; 2
